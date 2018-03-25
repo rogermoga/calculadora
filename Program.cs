0cdbac0edb070b5def9143e71d87e135d06f8cec
Roger Moga <rogermoga@hotmail.com>
2018-03-25 14:00:03 +0200

    He limpiado el codigo y lo he estructurado en funciones

--- Program.cs
+++ Program.cs
@@ -8,54 +8,64 @@ namespace PooVueling
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             Icalculadora ICalculadora = new Calculadora();
 
-            int input = 0;
-
-            while(input != 5) { 
-            Console.WriteLine("Que opcion desea realizar:");
-            Console.WriteLine("1) Sumar");
-            Console.WriteLine("2) Restar");
-            Console.WriteLine("3) Multiplicar");
-            Console.WriteLine("4) Dividir");
-            Console.WriteLine("5) Salir");
-
-            input = Convert.ToInt32(Console.ReadLine());
-
-                if(input != 5) { 
-            Console.WriteLine("Introduzca el primer valor:");
-            int value1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Introduzca el segundo valor");
-            int value2 = Convert.ToInt32(Console.ReadLine());
-                
-                switch (input)
+            int input;
+            do
             {
+                MuestraMenu();
+                input = Convert.ToInt32(Console.ReadLine());
 
-                case 1:
-                    Console.WriteLine("El resultado es: " + ICalculadora.Suma(value1, value2));
-                    break;
-                case 2:
-                    Console.WriteLine("El resultado es: " + ICalculadora.Resta(value1, value2));
-                    break;
-                case 3:
-                    Console.WriteLine("El resultado es: " + ICalculadora.Multiplicacion(value1, value2));
-                    break;
-                case 4:
-                    Console.WriteLine("El resultado es: " + ICalculadora.Division(value1, value2));
-                    break;
-                case 5:
+                if (input != 5)
+                {
+                    int value1, value2;
+                    LeeValores(out value1, out value2);
+                    Calcula(input, value1, value2); 
+                }
+                Console.WriteLine("");
+            } while (input != 5);
 
-                    break;
-                    
-             
-            }
+            void Calcula(int opcion, int value1, int value2)
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("El resultado es: " + ICalculadora.Suma(value1, value2));
+                        break;
+                    case 2:
+                        Console.WriteLine("El resultado es: " + ICalculadora.Resta(value1, value2));
+                        break;
+                    case 3:
+                        Console.WriteLine("El resultado es: " + ICalculadora.Multiplicacion(value1, value2));
+                        break;
+                    case 4:
+                        Console.WriteLine("El resultado es: " + ICalculadora.Division(value1, value2));
+                        break;
+                    case 5:
+                        break;
                 }
             }
 
+            void LeeValores(out int value1, out int value2)
+            {
+                Console.WriteLine("Introduzca el primer valor:");
+                value1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Introduzca el segundo valor");
+                value2 = Convert.ToInt32(Console.ReadLine());
+            }
+
+             void MuestraMenu()
+            {
+                Console.WriteLine("Que opcion desea realizar:");
+                Console.WriteLine("1) Sumar");
+                Console.WriteLine("2) Restar");
+                Console.WriteLine("3) Multiplicar");
+                Console.WriteLine("4) Dividir");
+                Console.WriteLine("5) Salir");
+            }
         }
     }
 }
